36a381c7ea76130a6250ef7e3a58668e1a497f8b
Daniel Mozo <danielmozo@MacBook-Air-de-Daniel.local>
2022-08-11 12:48:58 -0500

    Test implemented

---
+++ src/test/java/com/example/test/people/app/service/ActivityServiceTest.java
@@ -0,0 +1,70 @@
+package com.example.test.people.app.service;
+
+import com.example.test.people.app.cross.Const;
+import com.example.test.people.app.model.api.activity.Activity;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.client.ExpectedCount;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class ActivityServiceTest {
+
+    @Autowired
+    private ActivityService activityService;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private MockRestServiceServer mockServer;
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @BeforeEach
+    public void init() {
+        mockServer = MockRestServiceServer.createServer(restTemplate);
+    }
+
+    @Test
+    public void call_get_activity_api() throws URISyntaxException, JsonProcessingException {
+        Activity activity = new Activity("Activity name",
+                "educational", 1, 0.0, "", "", 100.0);
+
+        mockServer.expect(ExpectedCount.once(),
+                        requestTo(new URI(Const.ACTIVITY_URL)))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withStatus(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(mapper.writeValueAsString(activity))
+                );
+
+        Activity response = activityService.getRandomActivity();
+        mockServer.verify();
+        Assertions.assertEquals(activity, response);
+    }
+}
